ae1ac206c6833ec27ef71268ff18ed974fee8369
Berke Parıldar <94047726+berkeparildar@users.noreply.github.com>
2025-03-17 02:19:59 +0300

    update

--- src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { StudentInfoInputComponent } from './student-info-input/student-info-input.component';
-import { ClassViewComponent } from './class-view/class-view.component';
-
-export const routes: Routes = [
-    { path: '', component: StudentInfoInputComponent },
-    { path: 'class/:className', component: ClassViewComponent } // Parameterized route
-];
